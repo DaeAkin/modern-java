502b0bebf8842c05700b530d562313d9b96c70f2
Donghyeon Min <mindonghyeon890@gmail.com>
2020-08-24 09:49:42 +0900

    추가 내용

---
+++ src/main/java/dev/donghyeon/modernjava/LambdaFeatures.java
@@ -0,0 +1,28 @@
+package dev.donghyeon.modernjava;
+
+public class LambdaFeatures {
+    int y = 50;
+
+    public static void main(String[] args) throws Exception {
+        int x = 50;
+
+        Thread t = new Thread() {
+            public void run() {
+                System.out.println("MyThread start.");
+
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("MyThread end. x=" + x);
+            }
+        };
+
+        t.start();
+
+        x++;
+        System.out.println("main end");
+    }
+}
